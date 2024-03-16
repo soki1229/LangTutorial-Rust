bcc46f39260d673ee63a210bb3148aeeb0f49d25
PhilipKim <woosukplay3@gmail.com>
2024-03-17 00:53:23 +0900

    Tutorial Study of Rust
    1. Rust omits keyword 'return'. However, it is required on escaping.
    2. Rust has no increment(++) or decrement(--) operator
    3. Rust has no ternary operator
    4. Difference in argument usage: Pass by Reference

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
+fn foo(input: &mut i32, times: &mut i32) -> bool
+{
+    *input *= *times;
+    println!("{}", format!(" = {}", *input));
+
+    if *input <= 0
+    {
+        *input = -99;
+        return false;
+    }
+    
+    *times += 1;
+    // *input = if *input >= 10000 { 0 } else { *input };
+
+    true
+}
+
+fn main()
+{    
+    let (mut input, mut times, mut ret_flag) = (0, 1, true);
+    // let (a, b) = (&input, &times);
+    while ret_flag && times <= 12 
+    {
+        print!("{}", format!("Calculation: {} * {}", input, times));
+        ret_flag = foo(&mut input, &mut times);
+    }
+
+    println!("--> {}", format!("Terminated without error - {ret_flag}"));
+
+    // assert_eq!(foo(foo(3)), 100);
+
     println!("Hello, world!");
 }
+
+
+/*
+Rust omits keyword 'return'. However, it is required on escaping.
+Rust has no increment(++) or decrement(--) operator
+Rust has no ternary operator
+Difference in argument usage: Pass by Reference 
+*/
